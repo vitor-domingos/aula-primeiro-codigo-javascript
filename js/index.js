627e69e86ed5c5723a3ad59722119d5e23ef504b
Vitor Domingos <vm486092@gmail.com>
2023-10-18 21:42:50 -0200

    adicionada a pasta js

---
+++ js/index.js
@@ -0,0 +1,8 @@
+// Exibe uma janela pop-up de alerta com a mensagem de boas-vindas
+window.alert("Olá, bem-vindo ao meu arquivo javascript")
+
+// Exibe uma janela pop-up de confirmação no navegador
+window.confirm("Você deseja continuar navegando no site?")
+
+// Exibe uma janela pop-up de entrada de texto no navegador para obter o filme favorito do usuário
+window.prompt("Qual é o seu filme favorito?")
